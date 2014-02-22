89523d60ceac6bfd60948a9f3b04608fded68f69
masih72 <m.afshin93@gmail.com>
2014-02-22 10:55:16 -0800

    Clients connected to server successfully.

--- Client/socket_manager.cpp
+++ Client/socket_manager.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-// #include <sys/types.h>
-// #include <sys/socket.h>
+//#include <sys/types.h>
+//#include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include "socket_manager.h"
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
 using namespace std;
 
@@ -18,16 +22,20 @@ Socket_manager::Socket_manager(Client* cli)
 		exit(0);
 	}
 
+	struct hostent *server;
+	server = gethostbyname(cli->get_ip_addr());
 	struct sockaddr_in srv_addr;
 	bzero((char*) &srv_addr, sizeof(srv_addr));
 	srv_addr.sin_family = AF_INET;
 	srv_addr.sin_addr.s_addr = inet_addr(cli->get_ip_addr());
+	bcopy((char*)server->h_addr, (char*)&srv_addr.sin_addr.s_addr, server->h_length);
 	srv_addr.sin_port = htons(cli->get_port_num());
 
+	//cout<<connect(sockfd, (struct sockaddr*) &srv_addr, sizeof(srv_addr)) ;
 	if (connect(sockfd, (struct sockaddr*) &srv_addr, sizeof(srv_addr)) < 0)
 	{
 		cerr << "error connecting!" << endl;
-		exit(0);
+		//exit(0);
 	}
 
 	while(1);
